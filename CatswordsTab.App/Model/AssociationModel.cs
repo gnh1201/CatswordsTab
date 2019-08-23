901b30fe5f3fb6e6d5abacb1af8c672954190c0b
Go Namhyeon <gnh1201@gmail.com>
2019-08-23 23:55:38 +0900

    Add support file assoiciation in windows registry

---
+++ CatswordsTab.App/Model/AssociationModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatswordsTab.App.Model
+{
+    class AssociationModel
+    {
+        public string Default { get; set; }
+        public string ContentType { get; set; }
+        public string PerceivedType { get; set; }
+    }
+}
---
+++ CatswordsTab.App/RegistryService.cs
@@ -0,0 +1,34 @@
+﻿using CatswordsTab.App.Model;
+using Microsoft.Win32;
+using System;
+
+namespace CatswordsTab.App
+{
+    class RegistryService
+    {
+        public static AssociationModel GetAssoiciationByExtension(string extension)
+        {
+            AssociationModel association = null;
+
+            RegistryKey rk = Registry.ClassesRoot;
+            string skName = String.Format(".{0}", extension);
+
+            try {
+                using (RegistryKey sk = rk.OpenSubKey(skName))
+                {
+                    association = new AssociationModel
+                    {
+                        Default = (string)sk.GetValue(null), // null means default value
+                        ContentType = (string)sk.GetValue("Content Type"), // attention: 'Content Type' correct in windows registry
+                        PerceivedType = (string)sk.GetValue("PerceivedType")
+                    };
+                }
+            } catch(Exception)
+            {
+                // nothing
+            }
+
+            return association;
+        }
+    }
+}

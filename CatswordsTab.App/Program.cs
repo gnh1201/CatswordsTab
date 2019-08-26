176b16feb9736701a728065ea816fb9ecf1ca207
Go Namhyeon <gnh1201@gmail.com>
2019-08-26 13:20:10 +0900

    add command line parser

--- CatswordsTab.App/Program.cs
+++ CatswordsTab.App/Program.cs
@@ -1,11 +1,18 @@
 ﻿using System;
 using System.Windows.Forms;
 using CatswordsTab.App.Winform;
+using CommandLine;
 
 namespace CatswordsTab.App
 {
     static class Program
     {
+        class Options
+        {
+            [Option('f', "filename", Required = false, HelpText = "Set File name or path.")]
+            public string FileName { get; set; }
+        }
+
         /// <summary>
         /// 해당 응용 프로그램의 주 진입점입니다.
         /// </summary>
@@ -14,7 +21,25 @@ namespace CatswordsTab.App
         {
             System.Windows.Forms.Application.EnableVisualStyles();
             System.Windows.Forms.Application.SetCompatibleTextRenderingDefault(false);
-            System.Windows.Forms.Application.Run(new Welcome());
+
+            ParserResult<Options> _ = Parser.Default.ParseArguments<Options>(args);
+            _.WithParsed<Options>(o =>
+                {
+                    if (string.IsNullOrEmpty(o.FileName))
+                    {
+                        System.Windows.Forms.Application.Run(new Welcome());
+                    }
+                    else
+                    {
+                        System.Windows.Forms.Application.Run(new Main(o.FileName));
+                    }
+                }
+            );
+            _.WithNotParsed<Options>(e =>
+                {
+                    System.Windows.Forms.Application.Run(new Welcome());
+                }
+            );
         }
     }
 }

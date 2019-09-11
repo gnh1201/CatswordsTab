148d6739b380ab5905aca0933eafd9cd845d83ff
Go Namhyeon <gnh1201@gmail.com>
2019-09-11 15:12:02 +0900

    fix

--- CatswordsTab.App/Winform/Welcome.cs
+++ CatswordsTab.App/Winform/Welcome.cs
@@ -30,7 +30,7 @@ namespace CatswordsTab.App.Winform
             }
             catch (Exception)
             {
-                return null;
+                return "";
             }
         }
 
